7de9f52d9c5fcecd7d0f3f1e8be2065b7d7200ac
Jordan Stoute <jordanstoute2001@gmail.com>
2022-09-14 23:57:45 -0500

    Cursor and Camera
    Added Basic WASD Cursor movement as well as a Camera that follows

--- Fire Emblem Clone/Assets/Scripts/CursorMovement.cs
+++ Fire Emblem Clone/Assets/Scripts/CursorMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CursorMovement : MonoBehaviour
 {
+    private Vector3 endPos;
+    private Vector3 startPos;
+    public float duration = 0.5f;
+    private float timeElapsed;
+    private bool moving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,44 @@ public class CursorMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!moving)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                startPos = transform.position;
+                endPos = new Vector3(startPos.x + 1f, startPos.y, startPos.z);
+                moving = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                startPos = transform.position;
+                endPos = new Vector3(startPos.x - 1f, startPos.y, startPos.z);
+                moving = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                startPos = transform.position;
+                endPos = new Vector3(startPos.x, startPos.y, startPos.z + 1f);
+                moving = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                startPos = transform.position;
+                endPos = new Vector3(startPos.x, startPos.y, startPos.z - 1f);
+                moving = true;
+            }
+        }
+        else
+        {
+            timeElapsed += Time.deltaTime;
+            float percentComplete = timeElapsed / duration;
+
+            transform.position = Vector3.Lerp(startPos, endPos, percentComplete);
+            if (percentComplete >= 1f)
+            {
+                moving = false;
+                timeElapsed = 0f;
+            }
+        }
     }
 }
