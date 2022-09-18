03e0793896f9a9a09d7b7dead6b5c79f76a7802d
Jordan Stoute <jordanstoute2001@gmail.com>
2022-09-18 00:41:47 -0500

    Current Tile Recalculated
    Hitbox no longer required to detect current tile

--- Fire Emblem Clone/Assets/Scripts/CursorMovement.cs
+++ Fire Emblem Clone/Assets/Scripts/CursorMovement.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class CursorMovement : MonoBehaviour
 {
+    public PlayGrid playGrid;
     public float duration = 0.5f;
     public float waitTime = 1f;
     public float slowDuration = 1.75f;
 
+    private Transform[,] gridArray;
     private Vector3 endPos;
     private Vector3 startPos;
     private float durationElapsed;
     private float waitTimeElapsed;
     private bool moving = false;
+    private bool firstMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gridArray = playGrid.Grid;
     }
 
     // Update is called once per frame
@@ -29,6 +32,9 @@ public class CursorMovement : MonoBehaviour
     {
         if (!moving)
         {
+            int[] currentIndex = playGrid.CurrentIndex;
+            int[] lastIndex = playGrid.CurrentIndex;
+
             // If all buttons are released, completely stop and reset movement.
             if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
             {
@@ -45,29 +51,38 @@ public class CursorMovement : MonoBehaviour
             float horizDir = 0f;
             float vertDir = 0f;
             startPos = transform.position;
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D) && (currentIndex[1] != gridArray.GetLength(1) - 1))
             {
                 horizDir = 1f;
                 moving = true;
+                playGrid.SetMaterial(currentIndex, playGrid.defaultMaterial);
+                currentIndex[1] += 1;
             }
-            else if (Input.GetKey(KeyCode.A))
+            else if (Input.GetKey(KeyCode.A) && (currentIndex[1] != 0))
             {
                 horizDir = -1f;
                 moving = true;
+                playGrid.SetMaterial(currentIndex, playGrid.defaultMaterial);
+                currentIndex[1] -= 1;
             }
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) && (currentIndex[0] != 0))
             {
                 vertDir = 1f;
                 moving = true;
+                playGrid.SetMaterial(currentIndex, playGrid.defaultMaterial);
+                currentIndex[0] -= 1;
             }
-            else if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKey(KeyCode.S) && (currentIndex[0] != gridArray.GetLength(0) - 1))
             {
                 vertDir = -1f;
                 moving = true;
+                playGrid.SetMaterial(currentIndex, playGrid.defaultMaterial);
+                currentIndex[0] += 1;
             }
+
             endPos = new Vector3(startPos.x + horizDir, startPos.y, startPos.z + vertDir);
-
-
+            if (playGrid.GetMaterial(currentIndex) != playGrid.selectedMaterial)
+                playGrid.SetMaterial(currentIndex, playGrid.selectedMaterial);
         }
         else
         {
--- Fire Emblem Clone/Assets/Scripts/PlayGrid.cs
+++ Fire Emblem Clone/Assets/Scripts/PlayGrid.cs
@@ -4,12 +4,64 @@ using UnityEngine;
 
 public class PlayGrid : MonoBehaviour
 {
+    public Material defaultMaterial;
+    public Material selectedMaterial;
+    public GameManager gameManager;
+    public GameObject cursor;
+    public GameObject mainPlayer;
+
+    public int mStart;
+    public int nStart;
+
     private int m; // Number of rows
     private int n; // Number of columns
     private Transform[,] grid;
+    private int[] currentIndex;
+    //private int[] lastIndex;
 
-    // Start is called before the first frame update
+    public int[] CurrentIndex
+    {
+        get
+        {
+            return currentIndex;
+        }
+        set
+        {
+            currentIndex = value;
+        }
+    }
+    /*public int[] LastIndex
+    {
+        get
+        {
+            return lastIndex;
+        }
+        set
+        {
+            lastIndex = value;
+        }
+    }*/
+
+    public Transform[,] Grid
+    {
+        get
+        {
+            return grid;
+        }
+    }
+
     void Start()
+    {
+        SetGrid();
+        currentIndex = new int[] { mStart, nStart };
+        //lastIndex = new int[] { mStart, nStart };
+        SetMaterial(currentIndex, selectedMaterial);
+
+        cursor.transform.position = new Vector3(grid[mStart, nStart].position.x, grid[mStart, nStart].position.y + gameManager.cursorHeight, grid[mStart, nStart].position.z);
+        mainPlayer.transform.position = new Vector3(grid[mStart, nStart].position.x, grid[mStart, nStart].position.y + gameManager.unitHeight, grid[mStart, nStart].position.z);
+    }
+
+    void SetGrid()
     {
         m = transform.childCount;
         n = transform.GetChild(0).childCount;
@@ -22,13 +74,15 @@ public class PlayGrid : MonoBehaviour
                 grid[i, j] = transform.GetChild(i).GetChild(j);
             }
         }
-
-        Debug.Log(grid);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Material GetMaterial(int[] x)
     {
-        
+        return grid[x[0], x[1]].gameObject.GetComponent<Renderer>().material;
+    }
+
+    public void SetMaterial(int[] x, Material material)
+    {
+        grid[x[0], x[1]].gameObject.GetComponent<Renderer>().material = material;
     }
 }
